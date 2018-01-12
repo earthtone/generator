644f69c6742c446357c2897f30c0da0bb7669a31
earthtone <earthlyillusion@gmail.com>
2018-01-12 12:22:54 +1100

    ⚡️ Update Version Number Output

--- bin/grayson.js
+++ bin/grayson.js
@@ -4,7 +4,7 @@ var chalk = require('chalk');
 var program = require('commander');
 
 program
-	.version('0.1.0')
+	.version('0.2.2')
 	.command('init <project-directory>', 'initialize project')
 	.command('gen [options', 'generate HTML from MD')
 	.parse(process.argv);
