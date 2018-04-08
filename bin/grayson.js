da7c6aa651f25f55a7c94a17eacd97ec87e0689f
earthtone <earthlyillusion@gmail.com>
2018-04-09 02:06:31 +1000

    Removed options from help output

--- bin/grayson.js
+++ bin/grayson.js
@@ -6,7 +6,4 @@ program
 	.version('2.0.3')
 	.command('init', 'initialize project in pwd')
 	.command('gen [options]', 'generate HTML from MD')
-	.option('-o, --output <output>', 'Directory for HTML output')
-	.option('-p, --pages <pages>', 'Directory of Markdown Files to be Ingested')
-	.option('-m, --meta <metadata>', 'Directory JSON Metadata')
 	.parse(process.argv);
